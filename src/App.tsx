cbc5963b78c2a337aed41d84ad80f8ec7e29f953
[dyad] <srv.236@gmail.com>
2026-04-04 15:54:34 +0530

    Reverted all changes back to version edbcb6e4f26ae432423e955abd8009d3f3202469

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,6 @@ import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import FarmDetail from "./pages/FarmDetail";
-import ProfileDetail from "./pages/ProfileDetail";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -27,7 +26,6 @@ const App = () => (
             <Route path="/login" element={<Login />} />
             <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/farm/:id" element={<FarmDetail />} />
-            <Route path="/profile/:id" element={<ProfileDetail />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
