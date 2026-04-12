044229bcd6efff92fff36979e966af917c9c0b00
[dyad] <srv.236@gmail.com>
2026-04-12 16:15:04 +0530

    [dyad] Removing the practitioner_since column from the profiles table schema. - wrote 1 file(s)

--- remove_profile_location.sql
+++ remove_profile_location.sql
@@ -1,5 +1,6 @@
--- Remove location columns from profiles table
+-- Remove location and practitioner_since columns from profiles table
 ALTER TABLE public.profiles DROP COLUMN IF EXISTS address;
 ALTER TABLE public.profiles DROP COLUMN IF EXISTS latitude;
 ALTER TABLE public.profiles DROP COLUMN IF EXISTS longitude;
 ALTER TABLE public.profiles DROP COLUMN IF EXISTS google_maps_url;
+ALTER TABLE public.profiles DROP COLUMN IF EXISTS practitioner_since;
