d53f5d9a5b28888b393979c71bc5a089f85773ea
[dyad] <srv.236@gmail.com>
2026-04-12 16:22:50 +0530

    [dyad] Removing farm_name and locations from the profiles table and updating the Producer type definition. - wrote 2 file(s)

--- remove_profile_location.sql
+++ remove_profile_location.sql
@@ -1,6 +1,9 @@
--- Remove location and practitioner_since columns from profiles table
-ALTER TABLE public.profiles DROP COLUMN IF EXISTS address;
-ALTER TABLE public.profiles DROP COLUMN IF EXISTS latitude;
-ALTER TABLE public.profiles DROP COLUMN IF EXISTS longitude;
-ALTER TABLE public.profiles DROP COLUMN IF EXISTS google_maps_url;
-ALTER TABLE public.profiles DROP COLUMN IF EXISTS practitioner_since;
+-- Remove redundant columns from profiles table
+ALTER TABLE public.profiles 
+DROP COLUMN IF EXISTS address,
+DROP COLUMN IF EXISTS latitude,
+DROP COLUMN IF EXISTS longitude,
+DROP COLUMN IF EXISTS google_maps_url,
+DROP COLUMN IF EXISTS practitioner_since,
+DROP COLUMN IF EXISTS farm_name,
+DROP COLUMN IF EXISTS locations;
